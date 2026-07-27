7cef75cfef687e2abe2a33115214fc6a60b63f5d
raghu <raghumsmp@gmail.com>
2026-07-27 17:47:23 +0530

    sets,maps and itterations

---
+++ loops/maps.js
@@ -0,0 +1,20 @@
+const map = new Map();
+
+map.set("a", 1);
+map.set("b", 2);
+map.set("c", 3);
+map.set("d", "raju");
+
+console.log(map.get("a"));
+
+map.set("a", 97);
+
+console.log(map.get("a"));
+
+console.log(map.size);
+
+map.delete("b");
+
+console.log(map.size);
+
+console.log(map);
